12440f9f874aaafde274660a8ce81b8f2ba7eae5
MartiBesozzi <martina.besozzi@estudiante.ceibal.edu.uy>
2025-05-25 15:02:17 -0300

    Agregando equipos

---
+++ Arreglos/Practico3B/Index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Copa uruguaya</title>
+</head>
+<body>
+    <?php
+    $equipo = (
+        [
+            "nombre" => "Nacional",
+             "escudo" => "nacional.png", 
+             "puntos" => 0,
+             "partidos" => 0,
+ 
+        ["nombre" => "Peñarol", "escudo" => "peñarol.png", "puntos" => 1], 
+    ]
+);
+    ?>
+ 
+    <table border="1">
+        <tr>
+            <th>Nombre</th>
+            <th>Escudo</th>
+            <th>Precio</th>
+        </tr>
+ 
+</table>
+</body>
+</html>
